0dd45723f8dd1a37ee0bbe097ac9bae4e8f23748
snakekn <nadavkempinski@ucsb.edu>
2026-05-19 13:31:10 -0700

    Revert "Merge remote-tracking branch 'collaborator/Jackson-R' into merge-from-collab"
    This reverts commit 59a897d96c499529db42650e2bde526442cd2b28, reversing
    changes made to 03c1e7e92f39ed5b6c19602ce183315aa40757aa.

--- misc/rds_to_qs.R
+++
@@ -1,6 +0,0 @@
-library(qs2)
-files <- list.files("data/processed", pattern = "\\.rds$", full.names = TRUE)
-for (f in files) {
-  obj <- readRDS(f)
-  qs2::qs_save(obj, gsub("\\.rds$", ".qs2", f))
-}
